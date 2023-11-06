2bb3ea3951c6d81c425e0c5176bcc26a3e55fad2
Nik Klimenko <nikklimenko89@gmail.com>
2023-11-06 17:34:11 +0200

    feat: added logic to check password strength

--- src/app/components/password-strength/password-strength.component.ts
+++ src/app/components/password-strength/password-strength.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnChanges, SimpleChange} from '@angular/core';
 
 @Component({
   selector: 'password-strength',
   templateUrl: './password-strength.component.html',
   styleUrls: ['./password-strength.component.scss']
 })
-export class PasswordStrengthComponent {
-  bar1!: string;
-  bar2!: string;
-  bar3!: string;
+export class PasswordStrengthComponent implements OnChanges{
+  bars: string[] = [];
+  @Input() passwordToCheck!: string;
+  private colors: string[] = ['darkred', 'orange', 'yellowgreen'];
+
+  ngOnChanges(changes: { [propName: string]: SimpleChange }): void {
+    const password = changes['passwordToCheck'].currentValue;
+    this.setBarColors(3, '#DDD');
+    if (password) {
+      const strength = this.checkStrength(password);
+      console.log(strength);
+      this.setColorForBars(strength);
+    }
+  }
+
+  checkStrength(password: string) {
+    const minPasswordLength: number = 8;
+    const regex = /[$-/:-?{-~!"^_@`\[\]]/g;
+    const letters = /[a-zA-Z]+/.test(password);
+    const numbers = /[0-9]+/.test(password);
+    const symbols = regex.test(password);
+    const flags = [letters, numbers, symbols];
+
+    let passedMatches = 0;
+    for (const flag of flags) {
+      passedMatches += flag ? 1 : 0;
+    }
+
+    return (password.length >= minPasswordLength) ? passedMatches : 0;
+  }
+  private setColorForBars(strength: number): void {
+    switch (strength) {
+      case 0:
+       return this.setBarColors(3, 'darkred');
+      case 1:
+        return this.setBarColors(1, 'darkred');
+      case 2:
+        return this.setBarColors(2, 'orange');
+      case 3:
+        return this.setBarColors(3, 'yellowgreen');
+      default:
+        return this.setBarColors(3, '#DDD');
+    }
+  }
+
+  private setBarColors(count: number, color: string) {
+    this.bars = Array(count).fill(color);
+  }
+
 }
